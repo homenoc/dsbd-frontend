5130ef59fcfe7f72a7bbf92b550bb069ce436fe5
symysak <yoshiaki.suyama@suyama.ne.jp>
2026-07-12 18:39:18 +0900

    chore: update dependencies

--- apps/admin/src/index.tsx
+++ apps/admin/src/index.tsx
@@ -7,7 +7,6 @@ import { SnackbarProvider, closeSnackbar } from 'notistack';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { queryClient } from './lib/queryClient';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 if (container) {
@@ -28,7 +27,3 @@ if (container) {
     </QueryClientProvider>,
   );
 }
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
